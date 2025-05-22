cdcabaee071a853b9b80f62fe4ed7a6a32f1abc0
Vit Benton <Vit@vitbenton.com>
2025-05-22 14:27:31 -0400

    search function created

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,20 @@
 import './App.css'
 import { useState } from 'react'
 import type { Repository } from './types/repository'
+import { fetchRepos } from './api'
 
 function App() {
   const [repositories, setRepositories] = useState<Repository[]>([])
   const [filterPopular, setFilterPopular] = useState(false)
+
+  const searchRepos = async () => {
+    try {
+      const reposFetch = await fetchRepos(filterPopular);
+      setRepositories(reposFetch);
+    } catch (error) {
+      console.error(error);
+    }
+  }
 
   return (
     <>
