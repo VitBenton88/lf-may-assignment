c1b33873e4808196c66b354493dd8aa3fe4cdaf1
Vit Benton <Vit@vitbenton.com>
2025-05-22 15:00:54 -0400

    search form installed

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import { fetchRepos } from './api'
 function App() {
   const [repositories, setRepositories] = useState<Repository[]>([])
   const [filterPopular, setFilterPopular] = useState(false)
+  const [searchKeyword, setSearchKeyword] = useState('')
 
   const searchRepos = async () => {
     try {
@@ -18,23 +19,45 @@ function App() {
 
   return (
     <>
+      <h1>GitHub Search</h1>
+      <form>
+        <label>
+          <input
+            type='checkbox'
+            checked={filterPopular}
+            onChange={e => setFilterPopular(e.target.checked)}
+          />
+          Popular
+        </label>
+        <input
+          type='text'
+          placeholder='Enter keyword'
+          value={searchKeyword}
+          onChange={e => setSearchKeyword(e.target.value)}
+        />
+        <button type='submit'>Search</button>
+      </form>
+
       {repositories.length ? (
-        <table>
-          <thead>
-            <tr>
-              <th>Name</th>
-              <th>Private</th>
-            </tr>
-          </thead>
-          <tbody>
-            {repositories.map(repo => (
-              <tr key={repo.id}>
-                <td>{repo.name}</td>
-                <td>{repo.isPrivate ? '✅' : '❌'}</td>
+        <>
+          <h2>Results:</h2>
+          <table>
+            <thead>
+              <tr>
+                <th>Name</th>
+                <th>Private</th>
               </tr>
-            ))}
-          </tbody>
-        </table>
+            </thead>
+            <tbody>
+              {repositories.map(repo => (
+                <tr key={repo.id}>
+                  <td>{repo.name}</td>
+                  <td>{repo.isPrivate ? '✅' : '❌'}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </>
       ) : (
         <p>Enter a keyword to search GitHub.</p>
       )}
