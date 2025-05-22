df00f0934c73e14e18b2534d84d35c1b4cee9a87
Vit Benton <Vit@vitbenton.com>
2025-05-22 16:18:10 -0400

    api fetch method logic fix

--- src/api.ts
+++ src/api.ts
@@ -1,18 +1,23 @@
 import type { Repository } from './types/repository'
 
-export const fetchRepos = async (searchKeyword = '', searchPopular = false): Promise<Repository[]> => {
+export const fetchRepos = async (searchKeyword = '', popularFilter = false): Promise<Repository[]> => {
   let fetchUrl = `https://api.github.com/search/repositories?q=${searchKeyword}`;
 
-  if (searchPopular) {
+  if (popularFilter) {
     fetchUrl += '+stars:>1000'
   }
 
-  const response = await fetch(fetchUrl)
-  const repos = await response.json()
+  const response = await fetch(fetchUrl);
 
-  return repos.map((repo: { id: string; name: string; private: boolean }) => ({
-    id: repo.id,
-    name: repo.name,
-    isPrivate: repo.private,
-  }))
+  if (response.ok) {
+    const { items } = await response.json();
+
+    return items.map((repo: { id: string; name: string; private: boolean }) => ({
+      id: repo.id,
+      name: repo.name,
+      isPrivate: repo.private,
+    }))
+  }
+
+  return [];
 }
