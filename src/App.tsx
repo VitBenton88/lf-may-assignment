091d39dd540ba912d3138728bf4a6fe4e3c61b36
Vit Benton <Vit@vitbenton.com>
2025-05-22 17:50:10 -0400

    route & component created for individual item

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import './App.css'
 import { Routes, Route } from 'react-router-dom'
 import Search from './components/Search'
+import Repository from './components/Repository'
 
 function App() {
   return (
     <Routes>
       <Route path='/' element={<Search />} />
+      <Route path='/repo/:id' element={<Repository />} />
     </Routes>
   )
 }
