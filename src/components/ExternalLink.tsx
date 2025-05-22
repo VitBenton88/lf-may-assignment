bfccf4449e1ff2cc19cbae671a931e48461ef10a
Vit Benton <Vit@vitbenton.com>
2025-05-22 19:39:19 -0400

    external link component created

---
+++ src/components/ExternalLink.tsx
@@ -0,0 +1,21 @@
+import { type FC, type ReactNode } from 'react'
+
+type ExternalLinkProps = {
+  children: ReactNode
+  href: string
+  target?: string
+}
+
+const ExternalLink: FC<ExternalLinkProps> = ({ children, href, target = '_blank' }) => {
+  return (
+    <a
+      href={href}
+      target={target}
+      rel="noopener noreferrer"
+    >
+      {children}
+    </a>
+  )
+}
+
+export default ExternalLink
