66676f6c653c0b46e2dfd2bd26f7162d5f36c6fd
Vit Benton <Vit@vitbenton.com>
2025-05-22 15:53:44 -0400

    loading state installed

--- src/App.tsx
+++ src/App.tsx
@@ -2,18 +2,24 @@ import './App.css'
 import { useState } from 'react'
 import type { Repository } from './types/repository'
 import { fetchRepos } from './api'
+import Loader from './components/Loader'
 
 function App() {
   const [repositories, setRepositories] = useState<Repository[]>([])
   const [filterPopular, setFilterPopular] = useState(false)
+  const [isLoading, setIsLoading] = useState(false)
   const [searchKeyword, setSearchKeyword] = useState('')
 
   const searchRepos = async () => {
+    setIsLoading(true);
+
     try {
       const reposFetch = await fetchRepos(searchKeyword, filterPopular);
       setRepositories(reposFetch);
     } catch (error) {
       console.error(error);
+    } finally {
+      setIsLoading(false);
     }
   }
 
@@ -26,46 +32,54 @@ function App() {
     <>
       <h1>GitHub Search</h1>
       <form onSubmit={handleSubmit}>
-        <label>
+        <fieldset disabled={isLoading}>
+          <label>
+            <input
+              type='checkbox'
+              checked={filterPopular}
+              onChange={e => setFilterPopular(e.target.checked)}
+            />
+            Popular
+          </label>
           <input
-            type='checkbox'
-            checked={filterPopular}
-            onChange={e => setFilterPopular(e.target.checked)}
+            type='text'
+            placeholder='Enter keyword'
+            value={searchKeyword}
+            onChange={e => setSearchKeyword(e.target.value)}
           />
-          Popular
-        </label>
-        <input
-          type='text'
-          placeholder='Enter keyword'
-          value={searchKeyword}
-          onChange={e => setSearchKeyword(e.target.value)}
-        />
-        <button type='submit'>Search</button>
+          <button type='submit'>Search</button>
+        </fieldset>
       </form>
 
-      {repositories.length ? (
-        <>
-          <h2>Results:</h2>
-          <table>
-            <thead>
-              <tr>
-                <th>Name</th>
-                <th>Private</th>
-              </tr>
-            </thead>
-            <tbody>
-              {repositories.map(repo => (
-                <tr key={repo.id}>
-                  <td>{repo.name}</td>
-                  <td>{repo.isPrivate ? '✅' : '❌'}</td>
-                </tr>
-              ))}
-            </tbody>
-          </table>
-        </>
-      ) : (
-        <p>Enter a keyword to search GitHub.</p>
-      )}
+      {isLoading ? (
+        <Loader />
+      ) : <>
+        {
+          repositories.length ? (
+            <>
+              <h2>Results:</h2>
+              <table>
+                <thead>
+                  <tr>
+                    <th>Name</th>
+                    <th>Private</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {repositories.map(repo => (
+                    <tr key={repo.id}>
+                      <td>{repo.name}</td>
+                      <td>{repo.isPrivate ? '✅' : '❌'}</td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+            </>
+          ) : (
+            <p>Enter a keyword to search GitHub.</p>
+          )
+        }
+      </>}
     </>
   )
 }
