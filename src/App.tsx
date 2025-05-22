f0635e0aa7abc3699270a8e1ae36dccfe8008f9c
Vit Benton <Vit@vitbenton.com>
2025-05-22 17:24:56 -0400

    search form HOC

--- src/App.tsx
+++ src/App.tsx
@@ -1,45 +1,9 @@
 import './App.css'
-import { useState, useCallback } from 'react'
-import type { Repository } from './types/repository'
-import { fetchRepos } from './api'
-import Loader from './components/Loader'
-import SearchForm from './components/Search/Form'
-import SearchResults from './components/Search/Results'
+import Search from './components/Search'
 
 function App() {
-  const [repositories, setRepositories] = useState<Repository[]>([])
-  const [hasSearched, setHasSearched] = useState(false)
-  const [isLoading, setIsLoading] = useState(false)
-
-  const handleFormSubmit = useCallback(async (searchKeyword: string, filterPopular: boolean) => {
-    setHasSearched(true);
-    setIsLoading(true);
-    setRepositories([]);
-
-    try {
-      const reposFetch = await fetchRepos(searchKeyword, filterPopular);
-      setRepositories(reposFetch);
-    } catch (error) {
-      console.error(error);
-    } finally {
-      setIsLoading(false);
-    }
-  }, [])
-
   return (
-    <>
-      <SearchForm onSubmit={handleFormSubmit} disableForm={isLoading} />
-
-      {!hasSearched && <p>Enter a keyword to search GitHub.</p>}
-
-      {hasSearched && (
-        isLoading ? (
-          <Loader />
-        ) : (
-          <SearchResults items={repositories} />
-        )
-      )}
-    </>
+    <Search />
   )
 }
 
---
+++ src/components/Search/index.tsx
@@ -0,0 +1,45 @@
+import { useState, useCallback, type FC } from 'react'
+import type { Repository } from '../../types/repository'
+import { fetchRepos } from '../../api'
+import Loader from '../Loader'
+import SearchForm from './Form'
+import SearchResults from './Results'
+
+const Search: FC = () => {
+  const [repositories, setRepositories] = useState<Repository[]>([])
+  const [hasSearched, setHasSearched] = useState(false)
+  const [isLoading, setIsLoading] = useState(false)
+
+  const handleFormSubmit = useCallback(async (searchKeyword: string, filterPopular: boolean) => {
+    setHasSearched(true);
+    setIsLoading(true);
+    setRepositories([]);
+
+    try {
+      const reposFetch = await fetchRepos(searchKeyword, filterPopular);
+      setRepositories(reposFetch);
+    } catch (error) {
+      console.error(error);
+    } finally {
+      setIsLoading(false);
+    }
+  }, [])
+
+  return (
+    <>
+      <SearchForm onSubmit={handleFormSubmit} disableForm={isLoading} />
+
+      {!hasSearched && <p>Enter a keyword to search GitHub.</p>}
+
+      {hasSearched && (
+        isLoading ? (
+          <Loader />
+        ) : (
+          <SearchResults items={repositories} />
+        )
+      )}
+    </>
+  )
+}
+
+export default Search
