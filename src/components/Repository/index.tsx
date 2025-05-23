3d717308ef11a5f77299052d22953e3df21c2682
Vit Benton <Vit@vitbenton.com>
2025-05-23 11:00:52 -0400

    nav home added to repo page

--- src/components/Repository/index.tsx
+++ src/components/Repository/index.tsx
@@ -11,14 +11,18 @@ const Repository: FC = () => {
   const { name, owner } = useParams<{ owner: string, name: string }>()
   const navigate = useNavigate()
 
-  if (!name || !owner) {
-    navigate('/')
-  }
-
   const formatDisplayDate = useCallback((isoString: string): string => {
     const date = new Date(isoString);
     return date.toLocaleString();
   }, [])
+
+  const handleNavigateHome = useCallback(() => {
+    navigate('/')
+  }, [])
+
+  if (!name || !owner) {
+    handleNavigateHome()
+  }
 
   useEffect(() => {
     const fetchRepository = async () => {
@@ -42,57 +46,60 @@ const Repository: FC = () => {
   if (!repository?.id) return (<p>Repository not found.</p>)
 
   return (
-    <main id="repository">
-      <header>
-        <h1>{repository.name}</h1>
-        {!!repository.description &&
-          (<h2>{repository.description}</h2>)
-        }
-        <h3>Owner: <ExternalLink href={repository.owner_url}>{repository.owner} &rarr;</ExternalLink></h3>
-        <h4>Created: {formatDisplayDate(repository.created_at)}</h4>
-        <h4>Updated: {formatDisplayDate(repository.updated_at)}</h4>
-      </header>
+    <>
+      <nav><button type='button' onClick={handleNavigateHome}>&larr; Back to search</button></nav>
+      <main id="repository">
+        <header>
+          <h1>{repository.name}</h1>
+          {!!repository.description &&
+            (<h2>{repository.description}</h2>)
+          }
+          <h3>Owner: <ExternalLink href={repository.owner_url}>{repository.owner} &rarr;</ExternalLink></h3>
+          <h4>Created: {formatDisplayDate(repository.created_at)}</h4>
+          <h4>Updated: {formatDisplayDate(repository.updated_at)}</h4>
+        </header>
 
-      <aside>
-        <section>
-          <header>
-            <h4>Details:</h4>
-          </header>
-          <ul>
-            <li>Language: {repository.language}</li>
-            <li>Size: {repository.size} bytes</li>
-            <li>{repository.has_downloads ? 'Has downloads' : 'No downloads'}</li>
-            <li>{repository.allow_forking ? 'Allows forking' : 'Does not allow forking'}</li>
-          </ul>
-        </section>
+        <aside>
+          <section>
+            <header>
+              <h4>Details:</h4>
+            </header>
+            <ul>
+              <li>Language: {repository.language}</li>
+              <li>Size: {repository.size} bytes</li>
+              <li>{repository.has_downloads ? 'Has downloads' : 'No downloads'}</li>
+              <li>{repository.allow_forking ? 'Allows forking' : 'Does not allow forking'}</li>
+            </ul>
+          </section>
 
-        <section>
-          <header>
-            <h4>Access:</h4>
-          </header>
-          <ul>
-            <li>{repository.archived ? 'Archived' : 'Not archived'}</li>
-            <li>{repository.isPrivate ? 'Private' : 'Public'}</li>
-          </ul>
-        </section>
+          <section>
+            <header>
+              <h4>Access:</h4>
+            </header>
+            <ul>
+              <li>{repository.archived ? 'Archived' : 'Not archived'}</li>
+              <li>{repository.isPrivate ? 'Private' : 'Public'}</li>
+            </ul>
+          </section>
 
-        <section>
-          <header>
-            <h4>Links:</h4>
-          </header>
-          <ul>
-            {!!repository.homepage &&
-              (<li>
-                <ExternalLink href={repository.homepage}>Homepage &rarr;</ExternalLink>
-              </li>)
-            }
-            <li>
-              <ExternalLink href={repository.html_url}>GitHub &rarr;</ExternalLink>
-            </li>
-          </ul>
-        </section>
-      </aside>
-    </main>
+          <section>
+            <header>
+              <h4>Links:</h4>
+            </header>
+            <ul>
+              {!!repository.homepage &&
+                (<li>
+                  <ExternalLink href={repository.homepage}>Homepage &rarr;</ExternalLink>
+                </li>)
+              }
+              <li>
+                <ExternalLink href={repository.html_url}>GitHub &rarr;</ExternalLink>
+              </li>
+            </ul>
+          </section>
+        </aside>
+      </main>
+    </>
   )
 }
 
