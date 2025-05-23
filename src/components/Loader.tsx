0381d6cb2e04d01a00006308845263f27a935a9f
Vit Benton <Vit@vitbenton.com>
2025-05-23 18:26:51 -0400

    lint fix

--- src/components/Loader.tsx
+++ src/components/Loader.tsx
@@ -1,5 +1,5 @@
 import type { FC } from 'react'
 
-const Loader: FC = () => <span className='loader' role='status' aria-label='Loading' data-testid='loader'></span>
+const Loader: FC = () => <span className="loader" role="status" aria-label="Loading" data-testid="loader"></span>
 
 export default Loader
